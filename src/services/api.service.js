c35d12565e7c3452e4314dcaa9ffe0a0d288ca74
yankyalvarezm <alvarezyanky7@gmail.com>
2024-09-10 16:41:58 -0400

    change a folder name

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,6 +1,7 @@
-const API_URL = "http://localhost:3000";
+// const API_URL = "http://localhost:3000";
 
 // const TEST_HOST = "http://localhost:5173";
 // const API_URL = "https://spotmeup-s.adaptable.app"
+const API_URL = "https://spotmeup-test.netlify.app"
 
 export { API_URL };
