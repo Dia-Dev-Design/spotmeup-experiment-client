8d75e8d822c441cc88920a991c446816059a7475
Dustin Hurtt <dustin_hurtt@outlook.com>
2024-11-27 15:51:44 -0500

    re-ran build command, commented out line 4 in main.jsx dustin3511272024

---
+++ build/assets/SpotMeUpIcon-DT8cF_H6.js
@@ -0,0 +1,1 @@
+import{u as e,j as o}from"./index-CLCtADo9.js";const a=()=>{const s=e(),t=()=>{s("/")};return o.jsx(o.Fragment,{children:o.jsx("div",{className:"SpotMeUp-MainIcon",onClick:t})})};export{a as S};
