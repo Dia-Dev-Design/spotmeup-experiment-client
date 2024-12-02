41bac8f52c27576669ce3b9de7fd8f79219711be
gerarmc <gerardomelendezpro@gmail.com>
2024-12-02 14:50:25 -0400

    added some functionality to the carousel on the MainEvents component gerardo2501222024

---
+++ build/assets/SpotMeUpIcon-Bq-2ML5R.js
@@ -0,0 +1,1 @@
+import{u as e,j as o}from"./index-B_bCliB4.js";const a=()=>{const s=e(),t=()=>{s("/")};return o.jsx(o.Fragment,{children:o.jsx("div",{className:"SpotMeUp-MainIcon",onClick:t})})};export{a as S};
